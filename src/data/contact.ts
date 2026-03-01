4678285517e8a1b6ba71b73ad9015ddba38a5390
Nilavann <nalasivam1997@gmail.com>
2026-03-01 18:22:18 +0530

    implementation: Embedded the map and location links.

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -6,6 +6,7 @@ export const contact = {
   emailHref: 'mailto:nconsultancy.tax@gmail.com',
   address: 'L-71, PCM Complex, Koottappalli Colony, Thiruchengodu, Namakkal – 637214, Tamil Nadu',
   mapsUrl: 'https://maps.app.goo.gl/sss917rKx2Zc5vqK6',
+  mapsEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3714.5197156167683!2d77.8694359!3d11.3689907!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba9614362551ca3%3A0x4b1f0369d4ba8d98!2sN%20Accounts%2C%20Tax%20%26%20GST%20Consultancy!5e1!3m2!1sen!2sin!4v1772366054453!5m2!1sen!2sin',
   officeHours: {
     weekdays: 'Mon – Sat: 9:00 AM – 6:00 PM',
     weekend: 'Sun: WhatsApp only',
